300c804689d9b9611480330cd39fbb64863ed3f9
Andrea <Bellotto>
2017-01-17 15:00:08 +0100

    added Locale extension and format number

--- UsefulExtensions/DoubleExtensions.swift
+++ UsefulExtensions/DoubleExtensions.swift
@@ -16,4 +16,14 @@ extension Double {
         let multiplier = pow(10.0, Double(fractionDigits))
         return (self * multiplier).rounded() / multiplier
     }
+    
+    // returns number formatted in what you need
+    
+    func format(with locale:Locale, and format:NumberFormatter.Style)->String{
+        let converted = NSDecimalNumber(decimal: Decimal(self))
+        let formatter = NumberFormatter()
+        formatter.locale = locale
+        formatter.numberStyle = format
+        return formatter.string(from: converted)!
+    }
 }
